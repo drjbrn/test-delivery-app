bc93e6fce516ca8a86e2366894eaaf2cc41bb2a9
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-05-25 20:58:12 +0300

    feat: add new types for project

--- src/type/type.d.ts
+++ src/type/type.d.ts
@@ -14,3 +14,35 @@ interface ProductsInitialState {
   allProducts: ProductsProps[];
   selectedShop: string;
 }
+
+interface CartItem {
+  id: number;
+  title: string;
+  price: number;
+  rating: number,
+  shop: string,
+  quantity: number;
+  totalPrice: number;
+}
+
+interface CartState {
+  items: CartItem[];
+  totalQuantity: number;
+  totalPrice: number;
+}
+
+interface PersonalInfo {
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+}
+
+interface PersonalInfoProps {
+  onDataSubmit: (data: PersonalInfo) => void;
+  isSubmitted: boolean;
+}
+
+interface ModalProps {
+  closeModal: () => void;
+}
