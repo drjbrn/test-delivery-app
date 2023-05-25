10a1e624d81ba8a3d3ca4945e48b8e62996def6b
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-05-25 21:01:25 +0300

    feat: add new Cart page to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import ShopPage from './pages/Shop';
+import Cart from './pages/Cart';
 import './App.scss';
 
 function App() {
   return (
-    <div>
+    <div className='app'>
+      <Link to='/' className='app__button'>
+        Shop
+      </Link>
+      <Link to='/cart' className='app__button'>
+        Cart
+      </Link>
       <Routes>
         <Route path='/' element={<ShopPage />} />
+        <Route path='/cart' element={<Cart />} />
       </Routes>
     </div>
   );
