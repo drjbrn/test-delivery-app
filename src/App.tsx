00e634fb6e021324332a7932cdccb7ba448df977
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-05-25 21:35:02 +0300

    feat: add amount items in cart to the Cart btn

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import { Routes, Route, Link } from 'react-router-dom';
 import ShopPage from './pages/Shop';
 import Cart from './pages/Cart';
 import './App.scss';
+import { useSelector } from 'react-redux';
+import { getCartItems } from './store/cartSlice';
 
 function App() {
+  const product = useSelector(getCartItems);
+  const amountItemsInCart = product.length;
+
   return (
     <div className='app'>
       <Link to='/' className='app__button'>
@@ -11,6 +16,8 @@ function App() {
       </Link>
       <Link to='/cart' className='app__button'>
         Cart
+        {' '}
+        {amountItemsInCart}
       </Link>
       <Routes>
         <Route path='/' element={<ShopPage />} />
