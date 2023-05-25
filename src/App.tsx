ccf998ab0adc785cad82bc6aed4c58929928550d
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-05-25 14:27:37 +0300

    feat: add new path to new page - ShopPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+import ShopPage from './pages/Shop';
 import './App.scss';
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path='/' element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
