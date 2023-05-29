d9e51fb92160d7c276538abd66d208631e2afbd7
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-05-29 16:06:53 +0300

    feat: add basename to Browser Router

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import './index.scss'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/test-delivery-app">
         <App />
       </BrowserRouter>
     </Provider>
